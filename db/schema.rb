a3537eab7298bb2b74552304cda4c8ea1c7ec6f4
cybsip <cyrielle.cyb@gmail.com>
2015-12-04 12:43:43 +0000

    ajout user a travel_log

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151204084640) do
+ActiveRecord::Schema.define(version: 20151204110416) do
 
   create_table "travel_logs", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
