e9c6ff5c8bc36add2d2cb8291d84eb1c2403a5aa
cybsip <cyrielle.cyb@gmail.com>
2016-01-19 15:37:06 +0000

    photo ok

--- app/models/travel_log.rb
+++ app/models/travel_log.rb
@@ -1,20 +1,15 @@
 class TravelLog < ActiveRecord::Base
+    before_save :rename_avatar, on: :create
     belongs_to :user
-    has_attached_file :avatar, 
-    :file_name => ':id/:normalize_basename',
-    :path => ':rails_root/app/assets/images/:normalize_basename.:content_type_extension',
-    
-    :styles => { :small => "230x230>" , :medium => "500x500>"}
+    has_attached_file :avatar, :styles => { :small => "230x230>" , :medium => "500x500>"}
     validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
-    
-    Paperclip.interpolates :normalize_basename do |attachment, style|
-        attachment.instance.normalize_basename
-    end
-
-
-      def normalize_basename
-        Time.now.to_i.to_s
-      end
 
     has_many :jours
+    
+    def rename_avatar
+      #avatar_file_name - important is the first word - avatar - depends on your column in DB table
+      extension = File.extname(avatar_file_name).downcase
+      self.avatar.instance_write :file_name, "#{Time.now.to_i.to_s}#{extension}"
+    end
+
 end
